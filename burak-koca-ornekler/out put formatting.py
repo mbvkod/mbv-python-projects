59e436a9b1b13bbd43e0816d9687bb4c695def6d
mbvkod <89793767+mbvkod@users.noreply.github.com>
2022-07-29 14:04:14 +0300

    cuma gunu çalışmalarım

---
+++ burak-koca-ornekler/out put formatting.py
@@ -0,0 +1,23 @@
+# print('hello')
+
+# print('{}'.format('hello'))
+# print('{}'.format(12))
+
+# print('hello {} world'.format('phyton'))
+
+# strval = 'phyton'
+# print('hello {} {}'.format(strval   , 'world'))
+
+# strval1 = 'hello'
+# strval2 = 'phyton'
+# strval3 = 'world'
+# print('{0} {1} {2}'.format(strval1, strval2, strval3))
+# print('{0} {2} {1}'.format(strval1, strval2, strval3))
+# print('{0} {0} {1} {2}'.format(strval1, strval2, strval3))
+# print('{:.15}'.format('hello phyton world'))
+# print('{:.5}'.format('hello phyton world'))
+
+# print('{:*>60}'.format('phyton world'))
+
+
+memoryview
